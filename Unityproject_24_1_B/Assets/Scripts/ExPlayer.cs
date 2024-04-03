98c18f54fc4b885027d74f84f66891061dfe486d
edaild <ch5183sb@gmail.com>
2024-04-03 14:54:06 +0900

    [Feat]게임리소스 추가, Trigger 사용

--- Unityproject_24_1_B/Assets/Scripts/ExPlayer.cs
+++ Unityproject_24_1_B/Assets/Scripts/ExPlayer.cs
@@ -42,7 +42,16 @@ public class ExPlayer : MonoBehaviour
         {
             point = 0;
             gameObject.transform.position = new Vector3(0.0f, 3.0f, 0.0f);
-            Debug.Log(collision.gameObject.name); //해당 오브젝트의 이름을 출력한다.  
+            Debug.Log(collision.gameObject.tag); //해당 오브젝트의 이름을 출력한다.  
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Item"))   // CompareTag 함수는 지어진 Tag(item)이름을 검사한다.
+        {
+            Debug.Log("아이템과 충돌함");
+            point += 10;
+            Destroy(other.gameObject);  // 파괴한다.
         }
     }
 }
